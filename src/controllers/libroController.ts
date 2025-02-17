ede9c66cc9bea47bd2c58d6b8ebda6e3791b46d9
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2025-02-17 18:00:45 -0500

    agrega paginación a la obtención de libros en el servicio de libros

--- src/controllers/libroController.ts
+++ src/controllers/libroController.ts
@@ -2,10 +2,12 @@ import { Response, Request } from "express";
 import { libroService } from "../services/libroService";
 
 export const obtenerLibros = async (req: Request, res: Response) => {
+  const page = parseInt(req.query.page as string, 10) || 1;
+  const limit = parseInt(req.query.limit as string, 10) || 15;
 
   try {
-    const libros = await libroService.obtenerLibros();
-    res.status(200).json(libros);
+    const { libros, totalPages } = await libroService.obtenerLibros(page, limit);
+    res.status(200).json({ libros, totalPages });
   } catch (error) {
     res.status(500).json({ error: "Error al obtener libros" });
   }
--- src/services/libroService.ts
+++ src/services/libroService.ts
@@ -1,11 +1,24 @@
 import { db } from "../db";
-import { eq, and } from "drizzle-orm";
+import { eq, desc } from "drizzle-orm";
 import { libros } from "../db/schema";
 
 export class LibroService {
 
-  async obtenerLibros() {
-    return await db.select().from(libros)
+  async obtenerLibros(page: number, limit: number) {
+    const offSet = (page - 1) * limit;
+
+    const librosPaginados = await db
+      .select()
+      .from(libros)
+      .orderBy(desc(libros.publicacion))
+      .limit(limit)
+      .offset(offSet);
+
+    const totalLibros = await db.get<{ count: number }>(`SELECT COUNT(*) as count FROM libros`);
+
+    const totalPages = Math.ceil(totalLibros.count / limit);
+
+    return { libros: librosPaginados, totalPages };
   }
 
   async obtenerLibroPorId(id: number) {
