2e2f627f777c173697ef569e36efd30461b4ddac
Juan Pablo Marquez Sanchez <juan013sanchez@gmail.com>
2025-02-27 15:10:29 -0500

    cambia el método de actualización de usuario a PUT y agrega pruebas para el historial y libros

--- src/routers/usuarioRoutes.ts
+++ src/routers/usuarioRoutes.ts
@@ -11,6 +11,6 @@ const usuarioRouter = Router()
 usuarioRouter.post('/usuario', crearUsuario)
 usuarioRouter.get('/usuario/:id', obtenerUsuarioPorId)
 usuarioRouter.post('/usuario/login', verificarUsuario)
-usuarioRouter.post('/usuario/actualizar', actualizarUsuario)
+usuarioRouter.put('/usuario/actualizar', actualizarUsuario)
 
 export default usuarioRouter
